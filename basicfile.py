abfe9df4a43e73306ed66dfeef2ceaddee0ad2f4
kvintessentsia <trinitikvintessentsia@yandex.ru>
2022-10-01 11:45:33 +0300

    6 готовый рабочий вариант

--- basicfile.py
+++ basicfile.py
@@ -27,14 +27,6 @@ def fishing(name, worms):
         for name_fish, count in catch.items():
             print(f'{name_fish} - {count}')
 
-if __name__ == '__main__':
-    proc = Process(target=fishing, kwargs=dict(name='Вася', worms=10))
-    proc.start()
-
-    fishing('Коля', worms=10)
-
-    proc.join()
-
 class Fisher(Process):
     def __init__(self, name, worms, *args, **kwargs):
         super().__init__(*args, **kwargs)
@@ -44,15 +36,25 @@ class Fisher(Process):
     def run(self):
         catch = defaultdict(int)
 
-        for worm in range(worms):
-            print(f'{name} Червяк номер {worm} - забросил, ждём...', flush=True)
+        for worm in range(self.worms):
+            print(f'{self.name} Червяк номер {worm} - забросил, ждём...', flush=True)
             _ = 3 ** (10000 * random.randint(70, 100))
             fish = random.choice(FISH)
             if fish is None:
-                print(f'Тьфу!У {name} Сожрали червяка', flush=True)
+                print(f'Тьфу!У {self.name} Сожрали червяка', flush=True)
             else:
                 print(f'Ура!! У меня {fish}', flush=True)
                 catch[fish] += 1
-            print(f'Итого, рыбак {name} поймал')
+            print(f'Итого, рыбак {self.name} поймал')
             for name_fish, count in catch.items():
                 print(f'{name_fish} - {count}')
+
+
+if __name__ == '__main__':
+
+    fisher_1 = Fisher('Vasya', 10)
+    fisher_2 = Fisher('Kolya', 10)
+    fisher_1.start()
+    fisher_2.start()
+    fisher_1.join()
+    fisher_2.join()
