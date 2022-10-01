03338511a94aa01ab3fe46ce3fa07fab473f8400
kvintessentsia <trinitikvintessentsia@yandex.ru>
2022-10-01 11:38:27 +0300

    5 создаю класс fisher, который является наследником мультипоточности. Тут я с помощью класса делаю процесс. Переписываю метод родителя run, потом создам объект этого класса, сколько угодно и спокойно буду рыбачить

--- basicfile.py
+++ basicfile.py
@@ -5,7 +5,7 @@ import os
 import random
 from collections import defaultdict
 from multiprocessing import Process, Pipe, Queue
-from Queue import empty
+from queue import Empty
 
 FISH = [None, 'плотва', 'окунь', 'лещ']
 
@@ -14,16 +14,16 @@ def fishing(name, worms):
     print(f'{name} process id:', os.getpid())
     catch = defaultdict(int)
 
-    for worm in worms:
+    for worm in range(worms):
         print(f'{name} Червяк номер {worm} - забросил, ждём...', flush=True)
-        _ = 3 ** (10000 * random.randint(70, 100))
         fish = random.choice(FISH)
         if fish is None:
+            _ = 3 ** (10000 * random.randint(70, 100))
             print(f'Тьфу!У {name} Сожрали червяка', flush=True)
         else:
-            print(f'Ура!! У меня {fish}' flush=True)
+            print(f'Ура!! У меня {fish}', flush=True)
             catch[fish] += 1
-        print(f'Итого, рыбак {name} поймал'):
+        print(f'Итого, рыбак {name} поймал')
         for name_fish, count in catch.items():
             print(f'{name_fish} - {count}')
 
@@ -34,3 +34,25 @@ if __name__ == '__main__':
     fishing('Коля', worms=10)
 
     proc.join()
+
+class Fisher(Process):
+    def __init__(self, name, worms, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.name = name
+        self.worms = worms
+
+    def run(self):
+        catch = defaultdict(int)
+
+        for worm in range(worms):
+            print(f'{name} Червяк номер {worm} - забросил, ждём...', flush=True)
+            _ = 3 ** (10000 * random.randint(70, 100))
+            fish = random.choice(FISH)
+            if fish is None:
+                print(f'Тьфу!У {name} Сожрали червяка', flush=True)
+            else:
+                print(f'Ура!! У меня {fish}', flush=True)
+                catch[fish] += 1
+            print(f'Итого, рыбак {name} поймал')
+            for name_fish, count in catch.items():
+                print(f'{name_fish} - {count}')
