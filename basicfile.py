c7ec3dbfb935f02123a5ad0484c40974fe784021
kvintessentsia <trinitikvintessentsia@yandex.ru>
2022-10-01 13:26:47 +0300

    7 checkpoint

--- basicfile.py
+++ basicfile.py
@@ -45,9 +45,9 @@ class Fisher(Process):
             else:
                 print(f'Ура!! У меня {fish}', flush=True)
                 catch[fish] += 1
-            print(f'Итого, рыбак {self.name} поймал')
-            for name_fish, count in catch.items():
-                print(f'{name_fish} - {count}')
+        print(f'Итого, рыбак {self.name} поймал')
+        for name_fish, count in catch.items():
+            print(f'{name_fish} - {count}')
 
 
 if __name__ == '__main__':
@@ -58,3 +58,40 @@ if __name__ == '__main__':
     fisher_2.start()
     fisher_1.join()
     fisher_2.join()
+
+#добавим функционал, добавим лодку, будем управлять процессами родительскими
+# и дочками
+class Fisher(Process):
+    def __init__(self, name, worms, conn, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.name = name
+        self.worms = worms
+        self.catched = 0
+        self.conn = conn
+    def run(self):
+        for worm in range(self.worms):
+            print(f'{self.name} Червяк номер {worm} - забросил, ждём...', flush=True)
+            _ = 3 ** (10000 * random.randint(70, 100))
+            fish = random.choice(FISH)
+            if fish is None:
+                print(f'У меня {self.fish} {self.name}', flush=True)
+                self.catched += 1
+        print(f'Итого, рыбак {self.name} поймал {self.catched}', flush=True)
+        self.conn.send(self.name, self.catched)
+        self.conn.close()
+
+if __name__ == '__main__':
+
+    humans = ['Васек', 'Колян', 'Петрович', 'Хмурый', 'Клава', ]
+    fishers, pipes = [], []
+    for name in humans:
+        parent_conn, child_conn = Pipe()
+    fisher = Fisher(name=name, worms=100, conn=child_conn)
+    fishers.append(fisher)
+    pipes.append(parent_conn)
+
+    for fisher in fishers:
+        fisher.start()
+    total_fish = 0
+    for conn in pipes:
+        name, fish_count = conn.recv()
